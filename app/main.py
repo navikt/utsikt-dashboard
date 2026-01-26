e86a44e31bf234f9073df9c615b018956d02b295
Luis Lopez <jorge.luis.lopez.marin@nav.no>
2026-01-26 15:30:02 +0100

    Adding BQ READ permission to app

--- app/main.py
+++ app/main.py
@@ -1,7 +1,27 @@
 import streamlit as st
 
-st.title("Hei")
+st.set_page_config(layout="wide")
 
-st.text("Dette er en test for å deploye en app")
 
-st.text("Dette er også en test")
+
+
+tab1, tab2 = st.tabs(["Faggruppe", "Ventestatus"])
+
+with tab1:
+    col1, col2, col3, col4= st.columns(4)
+    with col1:
+        faggruppe = st.multiselect("Faggruppe:", ["Alle", "test"], default="Alle")
+
+    with col2:
+        fagomrade = st.multiselect("Fagomrade:", ["Alle", "test"], default="Alle")
+
+    with col3:
+        oppløsning = st.selectbox("Oppløsning:", ["Alle", "test"], index=0)
+
+    with col4:
+        fra_dato = st.slider("Fra dato og til dato:", min_value=0, max_value=100, value=(10,100))
+
+
+
+with tab2:
+    st.text("Faggruppe")
