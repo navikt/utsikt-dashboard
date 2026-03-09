0f3613b1d21bd028c4fb60aa7b21fc438835330f
caroliwi <caroline.winther@nav.no>
2026-03-09 13:27:12 +0100

    :sparkles: lagt til egen mappe for tabs

--- app/main.py
+++ app/main.py
@@ -14,6 +14,8 @@ from enum import Enum
 from data import Table, Data
 from bq_connector import BigQueryConnector
 
+from tabs.om_dataen import om_dataen
+
 
 st.set_page_config(layout="wide")
 
@@ -407,23 +409,4 @@ with tab3:
     st.altair_chart(fig_ventestatus_manuell, width="stretch")
 
 with tab4:
-    st.header("Om dataen")
-    st.markdown(
-        """Datagrunnlaget for visualiseringene er hentet fra Oppdragssystemet  og omhandler beregninger.
-
-En beregning starter som et utbetalingsoppdrag fra fagssystem.  Etterhvert som Oppdragssystemet jobber med beregningen, vil beregningen brytes ned i mindre deler og være innom ulike steg. 
-
-
-#### Stoppnivå
- Stoppnivå er et begrep i Oppdragssystemet som brukes for nedbryting av beregninger. En beregning kan brytes ned i f.eks. perioder med tilhørende forfallsdato gjelde forskjellige mottakere eller kan gjelde ulike saker f.eks sykepenger til én bruker men gjelder flere forhold.
-
-#### Ventestatus
- Ventestatus er  status for stoppnivået,  altså hvor i beregningsløpet et stoppnivå er.  F.eks. et stoppnivå kan ha ventestatusen OVFO (for 'Overført til UR').
-
-#### Faggruppe
- En faggruppe består av flere fagområder hvor man ønsker en samlet felles skatt-og trekkberegning. F.eks. pensjonsrelaterte ytelser. 
-
-#### Fagområde
-Fagområde angir selve ytelsen f.eks. arbeidsavklaringspenger. 
-"""
-    )
+    om_dataen()
